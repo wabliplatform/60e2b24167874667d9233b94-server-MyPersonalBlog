ee129c7212485fa512cd8ce8a76fadcea4754c66
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2021-12-17 13:28:28 +0200

    My commit message

---
+++ models/Blog.js
@@ -0,0 +1,19 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const blogSchema = new Schema({
+Underscoreid:String , 
+
+date:String , 
+
+title:String , 
+
+posttext:String 
+
+
+})
+
+module.exports = {
+  Blog : mongoose.model('Blog', blogSchema),
+}
+
